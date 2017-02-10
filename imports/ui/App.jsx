48237991601dc65ccccb3879f2613664b4b43dd5
Mohammed A. Petiwala <mohammed.petiwala52@gmail.com>
2017-02-10 17:39:39 +0500

    trying to speedup meteor build

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,12 +1,18 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
-
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+var ReactBootstrap = require('react-bootstrap');
+var Button = ReactBootstrap.Button;
 // App component - represents the whole app
 class App extends Component {
     
   render() {
     return (
-      <div><h1>HELLO WORLD</h1></div>
+      <div>
+      	<h1>HELLO WORLD-3</h1>
+      	 <Button bsStyle="primary">Primary</Button>
+      </div>
     );
   }
 }
