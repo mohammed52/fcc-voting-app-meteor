632eb53c511fd4281bf1bbdcd2fe43e64f7768ae
mohammed5253 <mohammed.petiwala52@gmail.com>
2017-02-11 09:28:18 +0500

    synching

--- imports/ui/TestArea.js
+++ imports/ui/TestArea.js
@@ -2,23 +2,19 @@ import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import TestArea from './TestArea'
 
 var ReactBootstrap = require('react-bootstrap');
 var Button = ReactBootstrap.Button;
 // App component - represents the whole app
-class App extends Component {
+class TestArea extends Component {
     
   render() {
     return (
       <div>
-      	<h1>HELLO WORLD-22</h1>
-      	<h1>HELLO WORLD-23</h1>
-      	 <Button bsStyle="primary">Primary</Button>
-      	 <TestArea />
+      	Is it Ok-5 ?
       </div>
     );
   }
 }
  
-export default App
+export default TestArea
